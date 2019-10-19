0231ea7dc244a64d114de3c527411f8d87dd137f
Toeyty <61030236@kmitl.ac.th>
2019-10-19 12:31:20 +0700

    commit

--- Week07/Week07/Program.cs
+++ Week07/Week07/Program.cs
@@ -10,8 +10,33 @@ namespace Week07
     {
         static void Main(string[] args)
         {
-
+            Person p = new Professor();
+            p.SayHi();
+            Console.ReadKey();
 
         }
+    }
+}
+class Person
+{
+    public virtual void SayHi()
+    {
+        Console.WriteLine("Hi I'm a person");
+    }
+}
+
+class Student : Person
+{
+    public override void SayHi()
+    {
+        Console.WriteLine("Hi I'm a student");
+    }
+}
+
+class Teacher : Person
+{
+    public new void SayHi()
+    {
+        Console.WriteLine("Hi I'm a teacher");
     }
 }
