98fb0c3c816813e568f2d306bdc9b43a317e69b1
Toeyty <61030236@kmitl.ac.th>
2019-10-19 11:45:05 +0700

    02

--- Week07/Week07/Program.cs
+++ Week07/Week07/Program.cs
@@ -10,7 +10,34 @@ namespace Week07
     {
         static void Main(string[] args)
         {
+            
+
 
         }
     }
+        
+    
 }
+class Student
+{
+    private string name;
+    private string id;
+    private float gpa;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public string ID
+    {
+        get { return id; }
+        set { id = value; }
+    }
+    public float GPA
+    {
+        get { return gpa; }
+        set { gpa = value; }
+    }
+}
+
+
