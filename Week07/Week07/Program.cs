528bc48a7a2e11ba9c0cad786dfaa18f50ec979f
Toeyty <61030236@kmitl.ac.th>
2019-10-19 12:24:53 +0700

    11

--- Week07/Week07/Program.cs
+++ Week07/Week07/Program.cs
@@ -10,7 +10,17 @@ namespace Week07
     {
         static void Main(string[] args)
         {
-
+            Person p = new Person();
+            p.SayHi();
+            Student s = new Student();
+            s.SayHi();
+            Person sp = new Student();
+            sp.SayHi();
+            Teacher t = new Teacher();
+            t.SayHi();
+            Person tp = new Teacher();
+            tp.SayHi();
+            Console.ReadKey();
         }
     }
 }
